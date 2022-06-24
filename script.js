7902fe38ae91981e85939bbb435aaa1db01f5390
bittoMazza <robertomazza99@gmail.com>
2022-06-24 15:44:18 +0200

    add comments

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ qui dentro aggiungiamo il toggle('active') e stampiamo il log passando l'indice
 come argomento della funzione;
 */
 
-// SECONDA PARTE ESERCIZIO 
-
-/* Per creare le bombe dobbiamo fare una funzione che viene ripetuta 16 volte
-*/
 
 let blackList = [];
 const generateButton = document.getElementById('generate-grid-btn');
@@ -23,11 +19,13 @@ let bombNumber;
 let totPoints = 0;
 generateButton.addEventListener('click',function(){
     // RESET 
+    totPoints = 0;
     finalResult.innerHTML = ""
     gridWrapper.innerHTML = "" // Text Content = "" per rimuovere la vecchia tabella in modo che la nuova non si aggiunga alla vecchia 
     gridWrapper.classList.remove('end-game')
     console.clear();
     blackList = [""];
+    gridWrapper.classList.add('border-black');
 
     const diffSelection = document.getElementById('difficult-selection').value; // Prendo il valore scelto dall'utente
     let nameSquareClass; // Variabile a cui assegno il nome della classe da utilizzare per le box
@@ -43,15 +41,16 @@ generateButton.addEventListener('click',function(){
         numBox = 49;
         nameSquareClass = 'square-hard'
     }
-    gridWrapper.classList.add('border-black')
+
     // Popoliamo il nostro array
     for(let i = 0; i < 16 ; i++){
         bombNumber = generateRandomBomb(blackList,1,numBox);
         blackList.push(bombNumber);
        }
+
+    // Creiamo i nostri quadrati 
     for(let i = 0 ; i < numBox ; i++){
        const newBox = createBox(nameSquareClass);
-       // Creiamo i nostri quadrati 
        newBox.addEventListener('click',function(){
         // Verifichiamo se all'interno dell'array c'è un numero uguale all'indice attuale + 1
         // Se c'è allora il quadrato sarò una bomba
@@ -78,7 +77,6 @@ generateButton.addEventListener('click',function(){
        gridWrapper.append(newBox);
     }
 })
-
 
 
 function createBox(squareType){
